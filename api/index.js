68c4d4bfc6935e7f5d06000888c55f55aa5b1fbf
Zaid <161572905+iammdzaidalam@users.noreply.github.com>
2024-10-11 21:44:38 +0530

    chore: Add missing .npmignore files for node_modules and coverage

---
+++ api/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./models/User');
+const bycrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = 'adwadagff31243rfefsdf21r324g3g';
+const cookieParser = require('cookie-parser');
+const salt = bycrypt.genSaltSync(10);
+
+app.use(cookieParser());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://ZaidAlam:Zaid786$@finblog.xyei5.mongodb.net/?retryWrites=true&w=majority&appName=Finblog');
+
+app.post('/register', async(req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userDoc = await User.create({username, password:bycrypt.hashSync(password,salt),});
+        res.json(userDoc);
+    } catch (e) {
+        res.status(400).json(e);
+    }
+
+});
+
+app.post('/login', async(req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bycrypt.compareSync(password, userDoc.password);
+    if(passOk){
+        //logged in
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) =>{
+            if(err) throw err;
+            res.cookie('token',token).json({
+                username,
+                id:userDoc._id,
+            });
+        });
+    }
+    else{
+        res.status(400).json('Invalid Password');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret,{}, (err, info) => {
+        if(err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token','').json('ok');
+});
+
+app.post('/post', (req, res) => {})
+
+app.listen(4000);
---
+++ client/src/Post.js
@@ -0,0 +1,12 @@
+export default function Post({title,summary,cover,content}){
+    return(
+        <div className="post">
+        <img src="" alt="testimg" />
+        <div className="texts">
+        <h2>Lorem ipsum dolor sit amet, consectetur.</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis tempora soluta ut aliquid inventore! Temporibus at ducimus totam hic fuga.</p>
+        </div>
+      </div>
+      
+    );
+}
